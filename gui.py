703cb59eeea4be2eadd444f77caa15c4d9336cf4
amanpreetpawa <79104433+amanpreetpawa@users.noreply.github.com>
2021-02-28 08:42:18 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,33 +1,18 @@
-
 import cv2
 
 from tkinter import *
 from tkinter import ttk
 from PIL import *
-
-#defining the interface structure
 root=Tk()
 root.geometry('1080x720')
 root.config(bg="#CCCCCC")
 root.title("SUDOCODE")
 
-#adding lables
 label=Label(root, text="ASL to Text Converter", font = "arial 20 bold",bg='#808080')
 label.pack()
 
-#creating button
-trans_btn=Button(root,text="Translate",bg = "blue")
-trans_btn.place(x = 540, y= 680)
-
-#definig camera stream (output) structure
-cam=Frame(root, bg="white", width=100, height=100,)
-cam.pack()
-camlab=Label(cam)
-camlab.pack()
-
-cap=cv2.VideoCapture(0)
-
 def video_stream():
+    
    
     cap = cv2.VideoCapture(0)
     while True:
@@ -37,6 +22,37 @@ def video_stream():
             break
 
 
-video_stream()
+vid_btn=Button(root,text="Enable Webcam",command=video_stream, bg = "#808080")
+vid_btn.place(x = 540, y= 620)
+
+
+cam=Frame(root, bg="white", width=100, height=100,)
+cam.pack()
+camlab=Label(cam)
+camlab.pack()
+
+cap=cv2.VideoCapture(0)
+
+f = open('output_modified.txt')
+text_msg = f.readlines()
+
+
+def onclick():
+    l = Label(root,text=text_msg)
+    l.grid(row=100,column=150)
+
+trans_btn=Button(root,text="Translate",command=onclick, bg = "#808080")
+trans_btn.place(x = 540, y= 680)
 
 root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
